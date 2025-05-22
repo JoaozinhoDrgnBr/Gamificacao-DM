d3265fc3dd7273b09e2071430c4d293734f812da
Joao Otávio <116232109+JoaozinhoDrgnBr@users.noreply.github.com>
2025-05-21 22:17:36 -0300

    Update settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -18,8 +18,8 @@ pluginManagement {
 
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.1.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.22" apply false
+    id("com.android.application") version "8.2.2" apply false  // Atualizar de "8.1.0" para "8.2.2"
+    id("org.jetbrains.kotlin.android") version "1.9.10" apply false  // Também atualize o Kotlin
 }
 
 rootProject.name = "gamificacao_dm"
